3785eadc3d0eb3e10a09becd32a7b51d4c266dc6
jeehoo0767 <jeehoo0767@naver.com>
2020-12-21 20:14:50 +0900

    Add Component

---
+++ redux/src/Components/AddNumberRoot.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddNumber from './AddNumber'
+function AddNumberRoot() {
+    return (
+        <div>
+            <h1>AddNumberRoot</h1>
+            <AddNumber />
+        </div>
+    )
+}
+
+export default AddNumberRoot
---
+++ redux/src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import AddNumberRoot from './AddNumberRoot'
+import AddNumber from './AddNumber'
+import DisplayNumberRoot from './DisplayNumberRoot'
+
+function App() {
+  return (
+    <div>
+      <h1>Root</h1>
+      <AddNumberRoot />
+      <DisplayNumberRoot />
+    </div>
+  )
+}
+
+export default App
