ec96ebfcc05a8985564a263bbf4531e103f17558
jeehoo0767 <jeehoo0767@naver.com>
2021-01-24 00:29:26 +0900

    typescript practice

---
+++ react_with_typescript/react-with-typescript/src/Greeting.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type GreetingsProps = {
+    name : string;
+};
+
+const Greetings : React.FC<GreetingsProps> = ({name}) => (
+    <div>Hello, {name}</div>
+);
+
+export default Greetings
+
+
---
+++ typescript-tutorial/src/practice.ts
@@ -0,0 +1,7 @@
+const message : string = 'hello world';
+console.log(message); // hello world
+
+let count = 0; // 숫자
+count += 1;
+// count = '문자열'; 이러면 에러 
+
