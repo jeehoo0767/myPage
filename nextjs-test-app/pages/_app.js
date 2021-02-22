d5e0fa3db7dc6cd88b42a06be0dc442a4f9aaf4e
jeehoo0767 <jeehoo0767@naver.com>
2021-02-22 23:33:21 +0900

    앱 컴포넌트

--- nextjs-test-app/pages/_app.js
+++ nextjs-test-app/pages/_app.js
@@ -5,3 +5,11 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+/**
+ 페이지 전환 시 레이아웃을 유지.
+ 페이지 전환 시 상태값을 유지.
+ componentDidCatch를 이용하여 커스텀 에러를 핸들링 가능
+ 추가적인 데이터를 페이지로 주입시켜 주는게 가능
+ 글로벌 CSS를 이곳에 선언
+ 
+ **/ 
