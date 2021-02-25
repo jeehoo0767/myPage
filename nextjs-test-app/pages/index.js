87d71595e6cceb7569010bc3675d17363f79405d
jeehoo0767 <jeehoo0767@naver.com>
2021-02-26 04:37:36 +0900

    상품 목록 렌더링 및 레이아웃

--- nextjs-test-app/pages/index.js
+++ nextjs-test-app/pages/index.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import ItemList from "../pages/src/component/ItemList";
+import { Header, Divider } from "semantic-ui-react";
 
 export default function Home() {
-  const [list, setList] = uLeState([]);
+  const [list, setList] = useState([]);
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
-  const getData = () => {
-    Axios.get(API_URL).then((response) => {
+  const getData = async () => {
+    await Axios.get(API_URL).then((response) => {
       console.log(response.data);
       setList(response.data);
     });
@@ -21,6 +23,11 @@ export default function Home() {
       <Head>
         <title>HOME | 박지후</title>
       </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list} />
     </div>
   );
 }
--- nextjs-test-app/pages/src/component/ItemList.js
+++ nextjs-test-app/pages/src/component/ItemList.js
@@ -1,17 +1,24 @@
+import { Grid } from "semantic-ui-react";
+import styles from "./ItemList.module.css";
 export default function ItemList(props) {
   return (
     <div>
       <Grid columns={3} divided>
         <Grid.Row>
-          <Grid.Column>
-            <Image src="/images/wireframe/media-paragraph.png" />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src="/images/wireframe/media-paragraph.png" />
-          </Grid.Column>
-          <Grid.Column>
-            <Image src="/images/wireframe/media-paragraph.png" />
-          </Grid.Column>
+          {props.list.map((item) => {
+            return (
+              <div className={styles.wrap}>
+                <Grid.Column>
+                  <img src={item.image_link} alt={item.name} />
+                  <strong className={styles.tit_item}>{item.name}</strong>
+                  <span className={styles.txt_info}>
+                    {item.category} {item.product_type}
+                  </span>
+                  <strong className={styles.num_price}>${item.price}</strong>
+                </Grid.Column>
+              </div>
+            );
+          })}
         </Grid.Row>
       </Grid>
     </div>
