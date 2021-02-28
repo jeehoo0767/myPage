ceda4bde6e8eef73343cea942f2ad433e090fb3a
jeehoo0767 <jeehoo0767@naver.com>
2021-02-28 18:40:49 +0900

    다이나믹라우트 페이지

--- nextjs-test-app/pages/view/[id].js
+++ nextjs-test-app/pages/view/[id].js
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router";
-
+import Axios from "axios";
+import { useEffect, useState } from "react";
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
-  return <p>Post : {id}</p>;
+  const [item, setItem] = useState({});
+  const API_URL = `http://makeup-api/herokuapp.com/api/v1/products/${id}.json`;
+  const getData = () => {
+    Axios.get(API_URL).then((response) => {
+      console.log(response.data);
+    });
+  };
+  useEffect(() => {
+    if (id && id > 0) {
+      getData();
+    }
+  }, [id]);
+  return <div></div>;
 };
 
 export default Post;
