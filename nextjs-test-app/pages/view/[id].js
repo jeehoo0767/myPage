5462aed7fdab620432fc223265cdba1108be4c87
jeehoo0767 <jeehoo0767@naver.com>
2021-03-01 20:29:29 +0900

    아이템 상세 페이지

--- nextjs-test-app/pages/view/[id].js
+++ nextjs-test-app/pages/view/[id].js
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
 import Axios from "axios";
 import { useEffect, useState } from "react";
+import Item from "../src/component/Item";
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
   const [item, setItem] = useState({});
-  const API_URL = `http://makeup-api/herokuapp.com/api/v1/products/${id}.json`;
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
   const getData = () => {
     Axios.get(API_URL).then((response) => {
-      console.log(response.data);
+      setItem(response.data);
     });
   };
   useEffect(() => {
@@ -16,7 +17,7 @@ const Post = () => {
       getData();
     }
   }, [id]);
-  return <div></div>;
+  return <Item item={item} />;
 };
 
 export default Post;
