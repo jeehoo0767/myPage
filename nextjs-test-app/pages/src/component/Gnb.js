af03b0a7155923938089550eba81f5fcb037d5a2
jeehoo0767 <jeehoo0767@naver.com>
2021-03-02 20:31:12 +0900

    gnb 라우트

--- nextjs-test-app/pages/src/component/Gnb.js
+++ nextjs-test-app/pages/src/component/Gnb.js
@@ -1,24 +1,28 @@
 import { Menu } from "semantic-ui-react";
-
+import { useRouter } from "next/router";
 export default function Gnb() {
   const { activeItem } = "home";
+  const router = useRouter();
+  const golink = (e, data) => {
+    if (data.name === "home") {
+      router.push("/");
+    } else if (data.name === "about") {
+      router.push("/about");
+    }
+  };
 
   return (
     <Menu inverted>
+      <Menu.Item name="home" active={activeItem === "home"} onClick={golink} />
       <Menu.Item
-        name="home"
-        active={activeItem === "home"}
-        // onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="messages"
+        name="about"
         active={activeItem === "messages"}
-        // onClick={this.handleItemClick}
+        onClick={golink}
       />
       <Menu.Item
         name="friends"
         active={activeItem === "friends"}
-        // onClick={this.handleItemClick}
+        onClick={golink}
       />
     </Menu>
   );
