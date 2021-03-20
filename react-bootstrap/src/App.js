a5236bf1207348722c8f1730eb338fb5127e5221
jeehoo0767 <jeehoo0767@naver.com>
2021-03-21 01:46:57 +0900

    grid style

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import ItemList from "./ItemList";
+import styles from "./ItemList.module.css";
 function App() {
   const [list, setList] = useState([]);
 
@@ -28,4 +29,30 @@ function App() {
   );
 }
 
+{
+  /* <Grid columns={3}>
+        <Grid.Row>
+          {list.map((item) => (
+            <Grid.Column key={item.id}>
+              <Link href="/detail/[id]" as={`/detail/${item.id}`}>
+                <a>
+                  <div className={styles.wrap}>
+                    <img
+                      src={item.image_link}
+                      alt={item.name}
+                      className={styles.img_item}
+                    />
+                    <strong className={styles.tit_item}>{item.name}</strong>
+                    <span className={styles.txt_info}>
+                      {item.category} {item.product_type}
+                    </span>
+                    <strong className={styles.num_price}>${item.price}</strong>
+                  </div>
+                </a>
+              </Link>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid> */
+}
 export default App;
