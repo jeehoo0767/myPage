32d0efbf1bb8437435082f34f11db271956f096c
jeehoo0767 <jeehoo0767@naver.com>
2021-03-26 00:59:22 +0900

    레이아웃

--- react-bootstrap/src/ItemList.js
+++ react-bootstrap/src/ItemList.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 function ItemList(props) {
-  const {list} = props;
+  const { list } = props;
   return (
     <div>
-       <Container>
+      <Container>
         <Row xs="3">
           {list.map((item) => {
             <Col key={item.id}>
               <div className={StyleSheet.wrap}>
-                <img src={item.image_link} alt={item.name} className={StyleSheet.img_item}/>
+                <img
+                  src={item.image_link}
+                  alt={item.name}
+                  className={StyleSheet.img_item}
+                />
                 <strong className={StyleSheet.tit_item}>{item.name}</strong>
                 <span className={styles.txt_info}>
                   {item.category} {item.product_type}
                 </span>
-                {/* <strong className={styles.num_price}>${item.price}</strong> */}
+                <strong className={styles.num_price}>${item.price}</strong>
               </div>
-            </Col>
+            </Col>;
           })}
         </Row>
-    </Container>
+      </Container>
       {/* <Grid columns={3}>
         <Grid.Row>
           {list.map((item) => (
