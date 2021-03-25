1b01efe4b0ebccb012a9f44b0d82ac3b0f6822f0
= <jeehoo0767@naver.com>
2021-03-25 18:07:21 +0900

    reactstrap grid 적용, prettier

--- react-bootstrap/src/ItemList.js
+++ react-bootstrap/src/ItemList.js
@@ -1,39 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from 'reactstrap';
 function ItemList(props) {
+  const {list} = props;
   return (
     <div>
        <Container>
-      <Row xs="2">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="3">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col xs="6">Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="1" sm="2" md="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
+        <Row xs="3">
+          {list.map((item) => {
+            <Col key={item.id}>
+              
+            </Col>
+          })}
+        </Row>
     </Container>
       {/* <Grid columns={3}>
         <Grid.Row>
